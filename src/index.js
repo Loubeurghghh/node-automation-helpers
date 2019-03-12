93beac57e5cf0fbd869f76942cb37c1dc13ca112
Lou Robinson <lourobinson@Lous-MacBook-Pro.local>
2019-03-12 13:57:48 +0000

    updated location of index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-const ApiHelper = require('../src/api_helper/api_helper');
-const JsonHelper = require('../src/json_helper/json_helper');
-const TestDataHelper = require('../src/test_data_helper/test_data_helper');
+const ApiHelper = require('./api_helper/api_helper');
+const JsonHelper = require('./json_helper/json_helper');
+const TestDataHelper = require('./test_data_helper/test_data_helper');
 
-module.exports = { ApiHelper, JsonHelper, TestDataHelper };
+module.exports = {
+    ApiHelper,
+    JsonHelper,
+    TestDataHelper
+};
