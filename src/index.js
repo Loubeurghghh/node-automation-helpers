1805f9e40ce91c9f3604abfa5217f434d3a74715
Lou Robinson <lou.robinson@filament.uk.com>
2019-03-28 18:41:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const ApiHelper = require('./api_helper/api_helper');
 const JsonHelper = require('./json_helper/json_helper');
-const SqlHelper = require('./sql_helper/json_helper');
+const SqlHelper = require('./sql_helper/sql_helper');
 const TestDataHelper = require('./test_data_helper/test_data_helper');
 
 module.exports = {
